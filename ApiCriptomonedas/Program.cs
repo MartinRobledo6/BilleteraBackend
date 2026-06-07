577dab018d992d0872ec6ce230cc3de6e79b9557
Martin Robledo <martinrobledosfco@gmail.com>
2026-06-07 19:55:00 -0300

    Cambios en TransaccionesServices

--- ApiCriptomonedas/Program.cs
+++ ApiCriptomonedas/Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-builder.Services.AddScoped<ITransaccioneService, TransaccionesServices>();
+builder.Services.AddHttpClient<ITransaccioneService, TransaccionesServices>();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
--- ApiCriptomonedas/Services/TransaccionesServices.cs
+++ ApiCriptomonedas/Services/TransaccionesServices.cs
@@ -3,16 +3,19 @@ using ApiCriptomonedas.DTOs;
 using ApiCriptomonedas.Interfaces;
 using ApiCriptomonedas.Model;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json;
 
 namespace ApiCriptomonedas.Services
 {
     public class TransaccionesServices : ITransaccioneService
     {
         private readonly AppDbContext _context;
+        private readonly HttpClient _httpClient;
 
-        public TransaccionesServices(AppDbContext context)
+        public TransaccionesServices(AppDbContext context, HttpClient httpClient)
         {
             _context = context;
+            _httpClient = httpClient;
         }
 
         public async Task<IEnumerable<Transacciones>> GetTransacciones()
@@ -24,12 +27,42 @@ namespace ApiCriptomonedas.Services
 
         public async Task<Transacciones> AddTransaccion(TransaccionesDTO transaccion)
         {
+            string cryptoCodeLower = transaccion.crypto_code.ToLower();
+
+            string urlCriptoya = $"https://criptoya.com/api/fiwind/{cryptoCodeLower}/ars";
+
+            decimal precio = 0;
+
+            try
+            {
+                var response = await _httpClient.GetAsync(urlCriptoya);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    using (JsonDocument doc = JsonDocument.Parse(jsonString))
+                    {
+                        precio = doc.RootElement.GetProperty("totalAsk").GetDecimal();
+                    }
+                }
+                else
+                {
+                    throw new Exception("No se pudo obtener el precio desde CryptoYa");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al consultar CryptoYa" + ex.Message);
+            }
+
+            decimal totalGastado = precio * transaccion.crypto_amount;
+
             var nuevaTransaccion = new Transacciones
             {
                 CryptoCode = transaccion.crypto_code,
                 Action = transaccion.action,
                 CryptoAmount = transaccion.crypto_amount,
-                Money = transaccion.money,
+                Money = totalGastado,
                 DateTime = transaccion.datetime,
             };
 
